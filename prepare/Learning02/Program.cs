12343f4f64feb1c52465ac2f6bf8488d7d7054c7
Justus <galaxycheesemc@gmail.com>
2024-05-08 19:11:19 -0700

    steps 1-6

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Microsoft";
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs[0]._jobTitle = "Chief Editor";
+        Console.WriteLine(myResume._jobs[0]._jobTitle);
     }
+
 }
