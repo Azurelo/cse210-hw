02f60b8faed214cfcd537b44874aff7df9caff27
Justus <galaxycheesemc@gmail.com>
2024-06-05 10:30:02 -0700

    setup

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,4 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
-    }
-}
