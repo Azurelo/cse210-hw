8718f648a457260f1b5fd94b7e817084a7f4172d
Justus <galaxycheesemc@gmail.com>
2024-06-05 10:32:57 -0700

    breathing activity

---
+++ prove/Develop04/Breathing.cs
@@ -0,0 +1,20 @@
+public class Breathing : Activity
+{
+    public BreathingActivity(int duration)
+        : base(duration, "Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
+    {
+    }
+
+    public void PerformActivity()
+    {
+        Start();
+        for (int i = 0; i < _duration / 6; i++) // Assuming 6 seconds per breath cycle
+        {
+            Console.WriteLine("Breathe in...");
+            PauseWithAnimation("", 3);
+            Console.WriteLine("Breathe out...");
+            PauseWithAnimation("", 3);
+        }
+        End();
+    }
+}
