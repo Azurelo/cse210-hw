b5f511e4bcc3c224c9682f04d505d799f1834840
Justus <galaxycheesemc@gmail.com>
2024-06-27 12:23:36 -0700

    finish quest class

---
+++ prove/Develop05/EternalQuest.cs
@@ -0,0 +1,18 @@
+public class EternalQuest : Quest
+{
+    public EternalQuest(string name, string description, int points)
+        : base(name, description, points)
+    {
+    }
+
+    public override void RecordEvent(ref int totalPoints)
+    {
+        totalPoints += Points;
+        Console.WriteLine($"{Name} event recorded! You earned {Points} points.");
+    }
+
+    public override bool IsComplete()
+    {
+        return false; // EternalQuest is never complete
+    }
+}
