587c7668b108292839f0080e509f0862d421463a
Justus <galaxycheesemc@gmail.com>
2024-07-10 16:38:39 -0700

    order class

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,51 @@
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        this.products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+        foreach (var product in products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        if (customer.IsInUSA())
+        {
+            total += 5; // Shipping cost in USA
+        }
+        else
+        {
+            total += 35; // Shipping cost outside USA
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in products)
+        {
+            label += $"{product.GetName()} ({product.GetProductId()})\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{customer.GetName()}\n{customer.GetAddress()}";
+    }
+}
