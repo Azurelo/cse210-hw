306db0671fcb4df29178405c45500b14d74ddc3c
Justus <galaxycheesemc@gmail.com>
2024-07-10 16:40:16 -0700

    good cs conventions

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -1,25 +1,25 @@
 class Address
 {
-    private string street;
-    private string city;
-    private string state;
-    private string country;
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
 
     public Address(string street, string city, string state, string country)
     {
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.country = country;
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
     public bool IsInUSA()
     {
-        return country.ToLower() == "usa";
+        return _country.ToLower() == "usa";
     }
 
     public override string ToString()
     {
-        return $"{street}\n{city}, {state}\n{country}";
+        return $"{_street}\n{_city}, {_state}\n{_country}";
     }
 }
