f0c40c6f9fcf1168ea80b956a1a1555cbb74fbb4
Justus <galaxycheesemc@gmail.com>
2024-07-17 10:45:57 -0700

    setup and main

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        var activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 3.0),  // miles
+            new Running("03 Nov 2022", 30, 4.8),  // kilometers
+            new Cycling("03 Nov 2022", 45, 20.0), // speed in mph
+            new Swimming("03 Nov 2022", 45, 20)   // laps
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
