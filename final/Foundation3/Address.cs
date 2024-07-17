a670724b114af2ce67c0980e299f5c4d4d06536e
Justus <galaxycheesemc@gmail.com>
2024-07-17 10:36:37 -0700

    best practices

--- final/Foundation3/Address.cs
+++ final/Foundation3/Address.cs
@@ -1,20 +1,20 @@
 class Address
 {
-    private string street;
-    private string city;
-    private string state;
-    private string country;
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
 
     public Address(string street, string city, string state, string country)
     {
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.country = country;
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
     public override string ToString()
     {
-        return $"{street}, {city}, {state}, {country}";
+        return $"{_street}, {_city}, {_state}, {_country}";
     }
 }
